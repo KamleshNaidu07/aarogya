9e053516ee0ad3d0339a073cd3a60f2a69f2326b
KamleshNaidu07 <kamleshnaidu07@gmail.com>
2023-12-02 16:41:39 +0530

    View Doctor datatable done

--- src/app/doctor/doctor.module.ts
+++ src/app/doctor/doctor.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewDoctorComponent } from './view-doctor/view-doctor.component';
 
-
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ViewDoctorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    
   ]
 })
 export class DoctorModule { }
