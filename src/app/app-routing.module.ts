c7bac41509a9563d48b2acb3a03ed634bc7a36a6
KamleshNaidu07 <kamleshnaidu07@gmail.com>
2023-12-02 15:37:56 +0530

    Added doctor view and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ViewPatientComponent } from './patient/view-patient/view-patient.component';
+import { ViewDoctorComponent } from './doctor/view-doctor/view-doctor.component';
 import { SummaryComponent } from './dashboard/summary/summary.component';
 import { LoginComponent } from './authenticate/login/login.component';
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'dashboard', component: SummaryComponent, canActivate: [AuthGuard] },
   { path: 'patient', component: ViewPatientComponent, canActivate: [AuthGuard] },
+  { path: 'doctor', component: ViewDoctorComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
 ];
 
---
+++ src/app/doctor/doctor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewDoctorComponent } from './view-doctor/view-doctor.component';
+
+
+
+@NgModule({
+  declarations: [
+    ViewDoctorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DoctorModule { }
